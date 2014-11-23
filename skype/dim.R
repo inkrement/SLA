ec3693bce5132e85e8754d3cfec85f2a905d23c3
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-11-23 10:53:11 +0100

    avl calculation

---
+++ skype/dim.R
@@ -0,0 +1,38 @@
+
+#
+# import values
+#
+
+values <- scan("start_times_only", what=integer())
+
+#aggregate(values, by=list(data$group), FUN=mean)[2]
+
+#
+# add minute column
+#
+minutes <- vector(length=length(values))
+hours <- vector(length=length(values))
+
+j = 1
+for(session in values) {
+	minutes[j] = ceiling(session/60)
+	hours[j] = ceiling(session/60/60)
+	j <- j + 1
+}
+
+
+
+Data <- data.frame(minute=minutes, hour=hours, values=values)
+
+
+
+
+#Data
+
+aggregate(values ~ hour, data=Data, FUN=length)
+
+#cat(values)
+
+#hist(values, breaks=12, plot=TRUE)
+
+#Sys.sleep(23)
