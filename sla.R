c23cf22710d737f4a1930cf35f7360b939ae6f0c
v5r <github@b8000h.org>
2015-01-15 09:41:11 +0100

    part 4

--- sla.R
+++ sla.R
@@ -31,6 +31,12 @@ availability <- function(en_saving) {
 	return(availability)
 }
 
+# inverse of above
+ensavings <- function(avail) {
+  overhead_ratio <- min(sd_dc)/max(sd_dc)
+  return((1-avail) / overhead_ratio);
+}
+
 for(location in locations) {
     el_price = prices[, location]
     temp = temperatures[, location]
@@ -61,7 +67,7 @@ for (i in range_en_savings) {
 sla_availabilities = list(0.931, 0.966, 0.981, 0.987, 0.992, 0.998)
 for (sla in sla_availabilities) {
     i <- match(FALSE, range_availability>sla)-1
-    cat("sla: ", sla, " (actual: ", range_availability[i], "), cost: ", range_costs[i], ", savings: ", range_en_savings[i], "\n")
+    #cat("sla: ", sla, " (actual: ", range_availability[i], "), cost: ", range_costs[i], ", savings: ", range_en_savings[i], "\n")
 }
 
 
@@ -77,30 +83,35 @@ for (sla in sla_availabilities) {
 ##
 # norm function 
 ##
+normalize_availability <- function(avail, minavail, maxavail) {
+  if(minavail == maxavail) {
+    return((avail>minavail)+0)
+  } else {
+    return((avail-minavail) / (maxavail-minavail))
+  }
+}
+
 normalize_availabilities <- function(availabilities){
-  return ((availabilities - min(availabilities) ) / (max(availabilities) - min(availabilities)) )
+  return(normalize_availability(availabilities, min(availabilities), max(availabilities)))
 }
 
 ##
 # prospect based satisfaction function
 ##
 pbsf <- function(n, w=0.5) {
-  #assert 0 <= n <= 1
-  stopifnot(n >= 0)
-  stopifnot(n <= 1)
-  
   if (n <= 0.5){
-    return(-0.5*(-2*n + 1) ^ (1-w) + 0.5)
-  } 
-  
-  return(0.5*(2 * n - 1) ^ (1-w) + 0.5)
+    sat <- -0.5*(-2*n + 1) ^ (1-w) + 0.5
+  } else {
+    sat <- 0.5*(2 * n - 1) ^ (1-w) + 0.5
+  }
+  return(max(0, min(sat, 1)))  # clamp to [0,1]
 }
 
 ##
 # payment willingness
 ##
-wtp <- function(avail, price) {
-	return(avail * price + rnorm(length(avail), sd=0.001))
+wtp <- function(avail, baseprice) {
+	return(avail * baseprice + rnorm(length(avail), sd=0.001))
 }
 
 ##
@@ -114,10 +125,10 @@ min_avail_skype <- 0.865
 max_amazon_aval <- 0.998
 
 aval_skype <- seq(min_avail_skype, max_amazon_aval, 0.001)
-user_scores <- lapply(normalize_availabilities(aval_skype), pbsf);
+user_scores_skype <- lapply(normalize_availabilities(aval_skype), pbsf);
 
 wtp_skype <- wtp(aval_skype, vm_baseprice)
-cat("Skype WTP:", wtp_skype)
+#cat("Skype WTP:", wtp_skype)
 
 ##
 #  microsoft
@@ -133,7 +144,7 @@ aval_ms <- seq(min_avail_ms, max_amazon_aval, 0.001)
 user_scores_ms <- lapply(normalize_availabilities(aval_ms), pbsf);
 
 wtp_ms <- wtp(aval_ms, vm_baseprice)
-cat("Microsoft WTP:", wtp_ms)
+#cat("Microsoft WTP:", wtp_ms)
 
 ##
 #  websites
@@ -149,30 +160,128 @@ aval_web <- seq(min_avail_web, max_amazon_aval, 0.001)
 user_scores_web <- lapply(normalize_availabilities(aval_web), pbsf);
 
 wtp_web <- wtp(aval_web, vm_baseprice)
-cat("Websites WTP:", wtp_web)
+#cat("Websites WTP:", wtp_web)
 
 
 plot(aval_ms, user_scores_ms, col="blue", type="l", xlab="availability", ylab="user score")
-lines(aval_skype, user_scores, col="orange")
+lines(aval_skype, user_scores_skype, col="orange")
 lines(aval_web, user_scores_web, col="red")
 
 legend(x=locator(n=1), legend=c("ms", "skype", "web"), fill=c("blue", "orange", "red"))
 locator(n=1)
 
 plot(aval_web, wtp_web)
+#locator(n=1)
 
-locator(n=1)
+# alternative utility function
+alpha = 60;
+beta = -0.015;
+gamma = 0.99;
 
-utility_model <- function(wtp, avt) {
-  return(wtp*(pbsf(avt)/0.5) - vm_baseprice)
+fi <- function(reqav, av) {
+  return(gamma/(gamma+beta*exp(reqav^2*alpha*(reqav-av))))
 }
 
-#util_web <- lapply(utility_model(aval_web, wtp_web, avail_web))
+utility_model <- function(wtp, reqavt, avt) {
+  return(wtp*(unlist(lapply(reqavt, function(ra) { pbsf(normalize_availability(avt, ra, max_amazon_aval)) }))/0.5) - unlist(lapply(avt, function (x) { costs(ensavings(x)) })))
+  #return(unlist(mapply(function(wp, reqav) { wp*fi(reqav, avt)-costs(ensavings(avt)) }, wtp, reqavt)))
+}
 
-vals <- c(utility_model(wtp_web, aval_web), utility_model(wtp_skype, aval_skype), utility_model(wtp_ms, aval_ms))
+#hist(utility_model(wtp_skype, aval_skype), xlab="price", ylab="#users", main="", breaks=10)
+#hist(utility_model(wtp_skype, aval_skype), xlab="price", title="", add=TRUE, density=10, breaks=10)
+#hist(utility_model(wtp_ms, aval_ms), xlab="price", title="", add=TRUE, density=10, angle=-45, breaks=10)
+#locator(n=1)
 
-hist(utility_model(wtp_web, aval_web), xlab="price", )
-locator(n=1)
-hist(utility_model(wtp_skype, aval_skype), xlab="price")
-locator(n=1)
-hist(utility_model(wtp_ms, aval_ms), xlab="price")
+#FIXME: should import this from proc.R
+availdata <- c(0.96685080256856, 0.992825395521464, 0.988075729359699, 
+  0.988783069297131, 0.992974908042774, 0.922675096162584, 0.973994960186689, 
+  0.9585829637388, 0.992669445712149, 0.988393682359168, 0.989098228980047, 
+  0.970510471090645, 0.989832088707599, 0.979780285565785, 0.97420158711781, 
+  0.982348305799232, 0.979270192796611, 0.965823503576895, 0.955838434702475, 
+  0.984487116558732, 0.934142357220784, 0.973078045462524, 0.989910314639284, 
+  0.981016225036856, 0.707718889113599, 0.96258011272433, 0.991582850623135, 
+  0.960646392838336, 0.985135708905142, 0.996821304625283, 0.991293069381962, 
+  0.990080689397929, 0.991248189836756, 0.994527178205465, 0.975772569444444, 
+  0.952592967284239, 0.966929813141927, 0.992591547641035, 0.956141410508575, 
+  0.856383018343798, 0.996796559592768, 0.987384006130062, 0.982598415094992, 
+  0.980817937010826, 0.974570843859803, 0.603593716233245, 0.972892148591434, 
+  0.986849964335785, 0.97485836843953, 0.990485813776329, 0.98145485778633, 
+  0.939699880601224, 0.984701816745656, 0.955542944517004, 0.96488850498029, 
+  0.959125024673736, 0.994223494821836, 0.99592943654555, 0.988719336931718, 
+  0.990602213205025, 0.99164472427229, 0.982294080217658, 0.937829464981691, 
+  0.987733905999085, 0.992588859208643, 0.984669178953835, 0.919595389789054, 
+  0.995282041919016, 0.988725015375154, 0.894392359385183, 0.993559375847088, 
+  0.982430587740358, 0.961137883228672, 0.934639293954156, 0.956554881496037, 
+  0.957653737171528, 0.909110832472288, 0.931957938153378, 0.994153446989644, 
+  0.98329986831067, 0.980041114243995, 0.993146469247266, 0.99212816301514, 
+  0.928584297839506, 0.988464378181499, 0.985816283728847, 0.981090843855716, 
+  0.979748427477479, 0.901137436625717, 0.986493995939766, 0.992136969699333, 
+  0.975128011275961, 0.933742227572459, 0.983402917996541, 0.976293256114221, 
+  0.990928589745603, 0.983865265495408, 0.987105329559417, 0.98988660210756, 
+  0.978748628751781, 0.983699085130255, 0.947303053853022, 0.991544617781288, 
+  0.992316623005965, 0.983069509326889, 0.930573781524311, 0.988699699403195, 
+  0.990381948628799, 0.957791908931253, 0.986690020867663, 0.965963138809614, 
+  0.986105406354222, 0.972867835216315, 0.987837762961422, 0.986456592411306, 
+  0.9854749810695, 0.980428932274696, 0.99188835101676, 0.645, 
+  0.6661, 0.6731, 0.6747, 0.6292, 0.4758, 0.4895, 0.6908148496038, 
+  0.998, 0.9094400062374, 0.7867222744556, 0.8932209429702, 0.4852040256432, 
+  0.5236998746532)
+
+
+maxclusters <- 15
+minconfidence = 0.95
+
+# matrix with 2 columns (avail and WTP)
+mat <- matrix(c(availdata, lapply(availdata, wtp, baseprice=vm_baseprice)), ncol=2)
+
+slausers <- matrix(nrow=maxclusters, ncol=maxclusters,
+                   dimnames=list(
+                     lapply(1:maxclusters, function(x) { paste0("sla", x) }),
+                     lapply(1:maxclusters, function(x) { paste0(x, "SLAs") })
+                   ))
+
+for (numclusters in seq(2, maxclusters, by=1)) {
+
+  clusters <- kmeans(mat, numclusters)
+  clmembers <- clusters[["cluster"]]
+  centers <- clusters[["centers"]]
+  satisfied <- vector(length=length(availdata))
+
+  clustermax <- vector(length=numclusters)              # maximum value of each cluster
+  numcustomers <- matrix(nrow=numclusters, ncol=2)
+  colnames(numcustomers) <- c("total", "positive_util")
+
+  for (i in seq(1, numclusters, by=1)) {
+    avails <- availdata[clmembers == i]
+    clustermax[i] <- max(avails)
+    ut <- utility_model(wtp(avails, vm_baseprice), avails, clustermax[i])
+    satisfied[clmembers == i] <- ut > 0
+    numcustomers[i,"total"] <- length(avails)
+    numcustomers[i,"positive_util"] <- length(ut[ut >= 0])
+  }
+
+  plot(mat, xlab="availability", ylab="WTP", pch=c(4, 19)[satisfied+1], col=clmembers)
+  #locator(n=1)
+
+  # result of clustering is unordered -> order by increasing SLA availability
+  ordering <- order(clustermax)
+  clustermax <- clustermax[ordering]
+  numcustomers <- numcustomers[ordering, ]
+
+  row.names(numcustomers) <- lapply(1:numclusters, function(x) { paste0("sla", x, "\n(", round(clustermax[x], digits=3), "%)") })
+
+  slausers[,numclusters] <- c(numcustomers[,"positive_util"], rep.int(0, maxclusters-numclusters))
+
+  confidence <- sum(numcustomers[,"positive_util"])/sum(numcustomers[,"total"])
+  if (confidence < minconfidence) {
+    cat("not enough confidence, have", confidence, "\n")
+  } else {
+    cat("confidence:", confidence, "\n")
+  }
+
+  cat(clustermax, "\n", numcustomers, "\n")
+}
+
+barplot(slausers, ylab="#users")
+
+#barplot(slausers[,dim(slausers)[[2]]], ylab="#users", main="sla selection")
